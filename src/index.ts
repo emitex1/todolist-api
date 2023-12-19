14c2f2e82b463e63981024172c86779638acdd46
Em-IT <emad.armoun@gmail.com>
2023-12-19 02:47:14 +0100

    change port to 3002

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import categoryRoutes from "./routes/category";
 import authorRoutes from "./routes/author";
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 app.use(express.json());
 
