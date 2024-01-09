28d6b9426e3e8bd3d0415b5e3112f7816955368b
Em-IT <emad.armoun@gmail.com>
2024-01-09 16:48:54 +0100

    allow netlify to CORS options

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,21 @@ import cors from "cors";
 const app = express();
 const port = 3002;
 
-app.use(cors());
+const corsOptions = {
+  origin: (origin: string | undefined, callback: any) => {
+    // Check if the origin is localhost or matches the netlify domain pattern
+    if (
+      !origin ||
+      origin === "http://localhost:3000" ||
+      origin.match(/^https:\/\/[a-z0-9-]+\.netlify\.app$/)
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
